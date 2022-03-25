008374ecfdaefacc6073a5d0ac194807678cecbb
Jase Seeley <jcseeley31590@yahoo.com>
2022-03-25 09:26:07 -0700

    add HomeController and Home Index.

---
+++ Pierres/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+using Pierres.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Pierres.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly PierresContext _db;
+
+    public HomeController(PierresContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      ViewBag.Treats = _db.Treats.ToList();
+      ViewBag.Flavors = _db.Flavors.ToList();
+      ViewBag.PageTitle = "Welcome to Pierre's Bakery!";
+      return View();
+    }
+  }
+}
